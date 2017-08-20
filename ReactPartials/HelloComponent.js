0e41e75593f542c279a7cff78abb4593f247f0fa
rohit <indians4german@gmail.com>
2017-08-20 22:26:22 +0530

    ejs data azure

--- ReactPartials/HelloComponent.js
+++ ReactPartials/HelloComponent.js
@@ -1,46 +1,12 @@
 var React = require("react")
 var ReactDOM = require('react-dom');
+var createReactClass = require('create-react-class');
+ 
 var _ = require('lodash')
-
-const Sequelize = require('sequelize');
-const sequelize = new Sequelize('demo', 'demo', 'Password@p', {
-	dialect: 'mssql',
-	host: 'rohitas.database.windows.net',	
-	dialectOptions: {
-		encrypt: true
-	}
-});
-
-sequelize.authenticate().then((err) => {
-	console.log('Connection successful with azure sql');
-})
-.catch((err) => {
-	console.log('Unable to connect to database', err);
-});                          //connection
- 
- 
- const Person = sequelize.define('Persons', {
-   PersonID: {
-	 type: Sequelize.INTEGER,
-	 primaryKey: true
-  },
-  FirstName: {
-    type: Sequelize.STRING
-  },
-  City: {
-    type: Sequelize.STRING
-  }
-}, {  timestamps: false ,} );  //model
  
 var tempstring ="hi kaaju"
- 
-Person.findAll().then(Person => {
-		tempstring = Person[3].dataValues;			 
-}) 
- 
- 
- 
-var HelloComponent = React.createClass({
+
+var HelloComponent = createReactClass({
 	
 	getInitialState: function() {
     return {
@@ -48,21 +14,15 @@ var HelloComponent = React.createClass({
       orderBy: 'petName',
       orderDir: 'asc',
       queryText: '',
-      myAppointments: []
+      myAppointments: tempstring
     } //return
   }, //getInitialState
-
-  componentDidMount: function() {
-    this.serverRequest = $.get('./js/data.json', function(result) {
-      var tempApts = result;
-      this.setState({
-        myAppointments: tempApts
-      }); //setState
-    }.bind(this));
-  }, //componentDidMount
-  
+    
+   
     render:function(){
-        return React.createElement('h1', null, tempstring);
+		 var myAppointments =  this.state.myAppointments  ;             	 
+         return React.createElement('h1', null, myAppointments)
+		//return myAppointments
     }
 });
 
--- server.js
+++ server.js
@@ -1,23 +1,60 @@
 var React = require("react"),
-    ReactDOMServer = require("react-dom/server"),     
-    myComponent =require("./HelloComponent"),
+    ReactDOMServer = require("react-dom/server"),
+    myComponent = require("./ReactPartials/HelloComponent"),
     express = require("express"),
     ReactComponent = React.createFactory(myComponent),
-    expressApp = express();
+    App = express();
 
-expressApp.set('views', __dirname + '/views');
-expressApp.set('view engine', 'ejs');
+App.set('views', __dirname + '/views');
+App.set('view engine', 'ejs');
 
-expressApp.get('', function(req, res){
-   var reactComponentMarkup = ReactComponent(),
-        staticMarkup = ReactDOMServer.renderToString(reactComponentMarkup);
-    //res.send(staticMarkup);
-    res.render('index', { helloComponentMarkup: staticMarkup })
+///////////// sql
+const Sequelize = require('sequelize');
+const sequelize = new Sequelize('demo', 'demo', 'Password@p', {
+    dialect: 'mssql',
+    host: 'rohitas.database.windows.net',
+    dialectOptions: {
+        encrypt: true
+    }
 });
 
-var expressServer = expressApp.listen(8080, function () {
+sequelize.authenticate().then((err) => {
+        console.log('Connection successful with azure sql');
+    })
+    .catch((err) => {
+        console.log('Unable to connect to database', err);
+    }); //connection
+
+
+const Person = sequelize.define('Persons', {
+    PersonID: {
+        type: Sequelize.INTEGER,
+        primaryKey: true
+    },
+    FirstName: {
+        type: Sequelize.STRING
+    },
+    City: {
+        type: Sequelize.STRING
+    }
+}, {
+    timestamps: false,
+}); //model
+
+///////////// sql
+//var reactComponentMarkup = ReactComponent(), staticMarkup = ReactDOMServer.renderToString(reactComponentMarkup);      
+
+
+App.get('/', function(req, res) {
+
+    Person.findAll().then(Person => {
+        res.render('index', {
+            alldata: Person
+        })
+    })
+});
+
+var expressServer = App.listen(8080, function() {
     var serverAddress = expressServer.address();
-    console.log("express server started...");
-    console.log("express server host name: ",serverAddress.address);
-    console.log("express listening to port no: ",serverAddress.port);
+    console.log("express listening to port no: ", serverAddress.port);
 });
